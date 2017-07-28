5822bee9c41847c1df64c92545f0727fcb219003
syjs10 <350172898@qq.com>
2017-07-28 09:41:20 +0800

    add/del/edit

--- etable/data.php
+++ etable/data.php
@@ -34,4 +34,39 @@
 		echo json_encode($data, true);
 		// var_dump($data);
 	}
+	function del_row(){
+		$dataid = $_POST['dataid'];
+		$sql   = "DELETE FROM `et_data` WHERE `id` = " . $dataid;
+		$query = query_sql($sql);
+		if ($query) {
+			echo "ok";
+		} else {
+			echo "false";
+		}
+	}
+	function add_row(){
+		$sql = 'INSERT INTO `et_data` (`c_a`, `c_b`, `c_c`, `c_d`, `c_e`, `c_f`, `c_g`, `c_h`) VALUES (';
+		for ($i=0; $i < 8; $i++) { 
+			$sql .= '\''.$_POST["col_" . $i ].'\', ';
+		}
+		$sql  = trim($sql, ", ");
+		$sql .= ");";
+		$res = query_sql($sql, "SELECT LAST_INSERT_ID() AS LD");
+		if ($res) {
+			$d = $res->fetch_assoc();
+			echo $d['LD'];
+ 		} else {
+			echo "false";
+		}
+	} 
+	function edit_row(){
+		$sql = "UPDATE `et_data` SET `c_a`='{$_POST['col_0']}', `c_b`='{$_POST['col_1']}', `c_c`='{$_POST['col_2']}', `c_d`='{$_POST['col_3']}', `c_e`='{$_POST['col_4']}', `c_f`='{$_POST['col_5']}', `c_g`='{$_POST['col_6']}', `c_h`='{$_POST['col_7']}' WHERE `id` = '{$_POST['dataid']}'";
+		$res = query_sql($sql);
+		if ($res) {
+			
+			echo "ok";
+ 		} else {
+			echo "false";
+		}
+	}
 
--- etable/js/test.js
+++ etable/js/test.js
@@ -36,59 +36,56 @@ $(function() {
         delButton.attr('dataid', data_item['id']);
         delButton.click(delHandler);
         var editButton = $('<a href="javascript:;" class="optLink">编辑 &nbsp;</a>');
-        editButton.click(editHandler);
+        // editButton.click(editHandler);	
         var opt_td = $('<td></td>');
         opt_td.append(delButton);
         opt_td.append(editButton);
         row_obj.append(opt_td);
         return row_obj;
     }
+    function editHandler(){
+   //  	var dataid = $(this).parent().children('a').attr('dataid');;
+    	
+   //  	var currentRow = $(this).parent().parent();
+    	
+   //  	var td_fields = currentRow.find("td");
+   //  	for (var i = 0; i < 8; i++) {
+   //  		var templ = '<input type="text" class="txtField" value="' + td_fields[i].innerText + '" />';
+			// td_fields[i].innerHTML=templ; 
+   //  	}
+    	
+   //  	td_fields[8].remove();
+   //      var col_opt = $('<td></td>');
+   //  	var confirmBtn = $('<a href="javascript:;" class="optLink">确认 &nbsp;</a>');
+   //      var cancelBtn = $('<a href="javascript:;" class="optLink">取消 &nbsp;</a>');
 
-    function editHandler() {
-        var dataid = $(this).parent().children('a').attr('dataid');;
-        var currentRow = $(this).parent().parent();
-        var data = [];
-        var td_fields = currentRow.find("td");
-        for (var i = 0; i < 8; i++) {
-            var templ = '<input type="text" class="txtField" value="' + td_fields[i].innerText + '" />';
-            data[i] = td_fields[i].innerText;
-            td_fields[i].innerHTML = templ;
-        }
+   //      confirmBtn.click(function() {
+   //      	var currentRow = $(this).parent().parent();
+   //          var input_fields = currentRow.find("input");
+   //          var post_fields = {};
+   //          for (var i = 0, j = input_fields.length; i < j; i++) {
+   //              post_fields['col_' + i] = input_fields[i].value;
+   //          }
+   //          post_fields['dataid'] = dataid;
+   //          $.post('data.php?action=edit_row', post_fields, function(res) {
+   //              if (res) {
+   //                  input_fields['id'] = dataid;
+   //                  var postAddRow = create_row(input_fields);
+   //                  currentRow.replaceWith(postAddRow);
+   //              } else {
+   //                  alert('添加失败');
+   //              }
+                
+   //          });
+   //      });
+   //      cancelBtn.click(function() {
+        	
+   //      });
+   //      col_opt.append(confirmBtn);
 
-        td_fields[8].remove();
-        var col_opt = $('<td></td>');
-        var confirmBtn = $('<a href="javascript:;" class="optLink">确认 &nbsp;</a>');
-        var cancelBtn = $('<a href="javascript:;" class="optLink">取消 &nbsp;</a>');
-
-        confirmBtn.click(function() {
-            var currentRow = $(this).parent().parent();
-            var input_fields = currentRow.find("input");
-            var post_fields = {};
-            for (var i = 0, j = input_fields.length; i < j; i++) {
-                post_fields['col_' + i] = input_fields[i].value;
-            }
-            post_fields['id'] = dataid;
-            $.post('data.php?action=edit_row', post_fields, function(res) {
-                if (res) {
-                    post_fields['id'] = dataid;
-                    var postAddRow = create_row(post_fields);
-                    currentRow.replaceWith(postAddRow);
-                } else {
-                    alert('添加失败');
-                }
-
-            });
-        });
-        cancelBtn.click(function() {
-            var tmp = create_row(data);
-            currentRow.replaceWith(tmp);
-        });
-        col_opt.append(confirmBtn);
-
-        col_opt.append(cancelBtn);
-        td_fields.parent().append(col_opt);
+   //      col_opt.append(cancelBtn);
+   //      td_fields.parent().append(col_opt);
     }
-
     function addHandler() {
         var new_line = $('<tr></tr>');
         for (var i = 0; i < 8; i++) {
@@ -107,9 +104,9 @@ $(function() {
             }
             $.post('data.php?action=add_row', post_fields, function(res) {
                 if (0 < res) {
-                    post_fields['id'] = res;
-                    var postAddRow = create_row(post_fields);
-                    currentRow.replaceWith(postAddRow);
+                    input_fields['id'] = res;
+                    var postAddRow = create_row(input_fields);
+                    // currentRow.replaceWith(postAddRow);
                 } else {
                     alert('添加失败');
                 }
