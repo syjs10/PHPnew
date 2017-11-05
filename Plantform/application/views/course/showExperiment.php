e01eea144275304cb0187e39539bd5ada8b6faae
syjs10 <350172898@qq.com>
2017-11-05 14:59:06 +0800

    update

--- Plantform/application/views/course/showExperiment.php
+++ Plantform/application/views/course/showExperiment.php
@@ -1,9 +1,8 @@
-
 <{block name=head}>
 <style type="text/css" media="screen">
 	div{
 		border: 1px solid #ccc;
-		height: 100%;  
+		height: 100%;
 	}
 	iframe{
 		width: 100%;
@@ -21,7 +20,7 @@
 			<iframe src="<{$base_url}>index.php/docker/show_docker"  id="noVNC"  onload="noVNC.focus()"></iframe>
 		</div>
 		<div class="col-md-3 right_bar">
-			<iframe src="<{$exp_info['exp_doc_path']}>"></iframe>
+			<iframe src="<{$exp_info['exp_doc_path']}>" height="90%"></iframe>
 		</div>
 	</div>
 </div>
