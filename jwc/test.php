456742b569b5ff70f7d920147e2473d38a7e1ca9
syjs10 <350172898@qq.com>
2018-01-28 18:48:31 +0800

    测试

--- jwc/test.php
+++ jwc/test.php
@@ -1,5 +1,5 @@
 <?php
-$output = shell_exec("python3 test.py 150402305 21103X 2");
+$output = shell_exec("python3 /home/syjs10/weixin/app/python/test.py 150402305 21103X 2");
 // var_dump($output);
 // var_dump();
-echo implode("|", json_decode($output, true));
+// echo implode("|", json_decode($output, true));
