d361d197f0af10feaea2200f1d46f472f6d4ee88
syjs10 <350172898@qq.com>
2018-02-10 21:19:18 +0800

    update

--- laravel_blog/app/Http/Controllers/ArticlesController.php
+++ laravel_blog/app/Http/Controllers/ArticlesController.php
@@ -17,17 +17,36 @@ class ArticlesController extends Controller
     public function create() {
     	return view('articles.create');
     }
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         $this->validate($request, [
             'title' => 'required|max:50',
         ]);
 
         $article = Article::create([
-            'title' => $request->title,
+            'title'   => $request->title,
             'content' => $request->content,
         ]);
 
         return redirect()->route('articles.index');
+    }
+    public function edit($id) {
+        $articles = Article::findOrFail($id);
+        return view('articles.edit', compact('articles'));
+    }
+    public function update(Request $request, $id) {
+        $this->validate($request, [
+            'title' => 'required|max:50',
+        ]);
+        $article = Article::findOrFail($id);
+        $article->update([
+            'title'   => $request->title,
+            'content' => $request->content,
+        ]);
+        return redirect()->route('articles.index');
+    }
+    public function destroy($id) {
+        $article = Article::findOrFail($id);
+        $article->delete();
+        return back();
     }
 }
